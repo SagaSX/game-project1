cdcaccdca8411910664b7e6970ac0ceffde9fa4c
Saga Mahyana <sagamahyana2@gmail.com>
2025-09-18 15:00:22 +0700

    Inisial Komit

--- Assets/Script/RespawnManager.cs
+++ Assets/Script/RespawnManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class RespawnManager : MonoBehaviour
 {
     public GameObject[] playerForms;
@@ -10,6 +10,9 @@ public class RespawnManager : MonoBehaviour
 
     private Vector3 currentCheckpoint;
     private GameObject currentPlayer;
+
+    public int maxDeath = 3;
+    public int currentDeath = 0;
 
     void Start()
     {
@@ -33,11 +36,24 @@ public class RespawnManager : MonoBehaviour
             Destroy(currentPlayer);
             Invoke(nameof(SpawnRandomPlayer), 1f);
             SFXManager.Instance.PlaySound(SFXManager.Instance.deathSound);
+
+            currentDeath++;
+
+            if (currentDeath>=maxDeath)
+            {
+                Invoke(nameof(RestartStage), 0.5f);
+            }
         }
     }
 
     public void SetCheckpoint(Vector3 newCheckpoint)
     {
         currentCheckpoint = newCheckpoint;
+    }
+
+    void RestartStage()
+    {
+        currentDeath = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
